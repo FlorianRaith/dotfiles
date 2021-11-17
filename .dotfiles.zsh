3796d85db35d670d960933de696551050f16aa49
Florian Raith <florianraith00@gmail.com>
2021-11-17 04:02:13 +0100

    include .dotfiles.zsh & remove .gitignore

---
+++ .dotfiles.zsh
@@ -0,0 +1,27 @@
+#            _       _    __ _ _                     _          
+#           | |     | |  / _(_) |                   | |         
+#         __| | ___ | |_| |_ _| | ___  ___   _______| |__       
+#        / _` |/ _ \| __|  _| | |/ _ \/ __| |_  / __| '_ \      
+#       | (_| | (_) | |_| | | | |  __/\__ \_ / /\__ \ | | |     
+#      (_)__,_|\___/ \__|_| |_|_|\___||___(_)___|___/_| |_|     
+#                                                              
+# add aliases for working and updating the dotfiles repo
+# also keep track of what files to include
+
+alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+alias dotfiles-update="dotfiles add \
+                       .config/i3 \
+                       .config/JetBrains \
+                       .config/kitty \
+                       .config/mpv \
+                       .config/neofetch \
+                       .config/nvim \
+                       .config/picom \
+                       .config/polybar \
+                       .config/ranger \
+                       .config/rofi \
+                       .config/zathura \
+                       .dotfiles.zsh \
+                       .p10k.zsh \
+                       .zshrc \
+                      && dotfiles commit -v"
